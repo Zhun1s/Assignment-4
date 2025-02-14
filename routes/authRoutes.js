c51b5721875e6308dce243aadfe0239f3d4a1279
Zhun1s <adil.zhunusov04@gmail.com>
2025-02-14 15:07:23 +0500

    fix again

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const multer = require("multer");
 const User = require("../models/User");
 
